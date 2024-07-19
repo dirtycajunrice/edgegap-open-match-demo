c9434fba979986d5f95ba5cbd45e956043d2cd44
Nicholas St. Germain <nick@cajun.pro>
2024-07-19 08:42:23 -0500

    remove csharp

--- tutorials/basic-components/csharp-http/director/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿using director;
-
-Director.Start();
--- tutorials/basic-components/csharp-http/scripts/build-images.ps1
+++
@@ -1,3 +0,0 @@
-docker build -t edgegap-om-tuto-frontend:tuto "$PSScriptRoot\..\front-end"
-docker build -t edgegap-om-tuto-match-function:tuto "$PSScriptRoot\..\match-function"
-docker build -t edgegap-om-tuto-director:tuto "$PSScriptRoot\..\director"
